5729b566109dc91ffeb30378df9c7426cce34539
RushikeshNagawade <rushikesh.nagawade@accenture.com>
2024-12-19 13:00:07 +0530

    Initial commit

---
+++ src/test/java/com/expense/tracker/category_service/CategoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expense.tracker.category_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
